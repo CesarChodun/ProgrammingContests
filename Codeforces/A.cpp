bdb44ece40409a7b67379323dafe28b907b7a23a
Cezary Choduń <c.chodun@student.uw.edu.pl>
2020-02-12 20:07:26 +0100

    Create A.cpp

---
+++ Codeforces/A.cpp
@@ -0,0 +1,80 @@
+    #include <bits/stdc++.h>
+     
+    #define ll long long
+    #define ll2 pair<ll, ll>
+    #define int2 pair<int, int>
+    #define mp make_pair
+    #define pb push_back
+    #define x first
+    #define y second
+    #define FORE(v) for (int i = 0; i < v.size(); ++i)
+    #define FOR(i, n) for(int i = 0; i < n; i++)
+    #define SORT(v) std::sort(v.begin(), v.end())
+    #define DPRINT if (DEBUG) std::cout
+    #define PRINT_V(v) {FORE(v) std::cout << v[i] << ", "; std::cout << "\n";}
+    #define DPRINT_V(v) if (DEBUG) { PRINT_V(v) }
+    #define vec vector
+     
+    #define MULTI_TASK true
+    #define DEBUG false
+    #define ALPHABET_SIZE 30
+     
+    using namespace std;
+     
+    const int SIZE = 1 * 1e5 + 70;
+    const ll MOD = 1e9 + 9;
+    
+    string s;
+     
+    void init() {
+        cin >> s;
+    }
+     
+    void prepare() {}
+     
+    void solve() {
+        int out = 0;
+        int ones = 0;
+        int zeros = 0;
+     
+        FOR(i, s.size()) {
+            char x = s[i];
+     
+            if (x == '0') {
+                zeros++;
+            }
+            else {
+                if (ones != 0)
+                    out += zeros;
+                ones++;
+                zeros = 0;
+            }
+        }
+     
+        cout << out << "\n";
+    }
+     
+    int main() {
+        if (!DEBUG) {
+            ios_base::sync_with_stdio(0);
+            cin.tie(0);
+        }
+     
+        if (MULTI_TASK) {
+            int t;
+            cin >> t;
+     
+            while (t--) {
+                init();
+                prepare();
+                solve();
+            }
+        }
+        else {
+            init();
+            prepare();
+            solve();
+        }
+     
+        return 0;
+    }
